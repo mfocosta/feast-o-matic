31bc99a3b66dca36c32cc874edea66ce4c112de6
Miguel Costa <86489930+mfocosta@users.noreply.github.com>
2025-11-20 01:58:53 +0000

    Remove unused variables

--- main/ota/ota.c
+++ main/ota/ota.c
@@ -18,13 +18,7 @@
 #include <sys/socket.h>
 #include <inttypes.h>
 
-#define HASH_LEN 32
-
 static const char *TAG = "ota_handler";
-extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
-extern const uint8_t server_cert_pem_end[]   asm("_binary_ca_cert_pem_end");
-
-#define OTA_URL_SIZE 256
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
